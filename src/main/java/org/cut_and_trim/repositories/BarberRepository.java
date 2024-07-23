668f4e123f6290e1a7d5c7ccba17d9847fb82385
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-23 15:11:20 -0300

    feat: criação do repository Barbeiro

---
+++ src/main/java/org/cut_and_trim/repositories/BarberRepository.java
@@ -0,0 +1,16 @@
+package org.cut_and_trim.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.cut_and_trim.models.Barber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BarberRepository extends JpaRepository<Barber, UUID>{
+    
+    Optional<Barber> findByLogin(String login);
+
+    Optional<Barber> findByEmail(String email);
+}
