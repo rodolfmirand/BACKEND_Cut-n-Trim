f2da06e244a9d7c53590fe98904c8c7b26d22c5f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-25 11:57:16 -0300

    fix: remodelação do banco H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,27 @@
-CREATE TABLE barber (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255),
-    username VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255)
+-- Criação da tabela BarberShop
+CREATE TABLE BarberShop (
+    id UUID PRIMARY KEY
 );
 
-CREATE TABLE barber_shop (
+-- Criação da tabela Barber
+CREATE TABLE Barber (
     id UUID PRIMARY KEY,
-    barber_id UUID,
-    FOREIGN KEY (barber_id) REFERENCES barber(id)
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    barberShop_id UUID UNIQUE,
+    CONSTRAINT fk_barberShop FOREIGN KEY (barberShop_id) REFERENCES BarberShop(id) ON DELETE CASCADE
 );
 
-CREATE TABLE client (
-    phone_number VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
+-- Atualização da tabela BarberShop para adicionar a relação bidirecional
+ALTER TABLE BarberShop
+ADD CONSTRAINT fk_barber FOREIGN KEY (id) REFERENCES Barber(id) ON DELETE CASCADE;
+
+-- Criação da tabela Client
+CREATE TABLE Client (
+    phoneNumber VARCHAR(255) PRIMARY KEY,
+    nome VARCHAR(255),
     barber_shop_id UUID,
-    FOREIGN KEY (barber_shop_id) REFERENCES barber_shop(id)
+    CONSTRAINT fk_client_barberShop FOREIGN KEY (barber_shop_id) REFERENCES BarberShop(id) ON DELETE CASCADE
 );
