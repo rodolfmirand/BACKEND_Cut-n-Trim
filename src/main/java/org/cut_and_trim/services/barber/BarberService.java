88ee2c206190a587f09db0e18c90bf17d54393a2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-25 12:02:55 -0300

    feat: criação das funções findById e findAll

--- src/main/java/org/cut_and_trim/services/barber/BarberService.java
+++ src/main/java/org/cut_and_trim/services/barber/BarberService.java
@@ -1,10 +1,17 @@
 package org.cut_and_trim.services.barber;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.cut_and_trim.dtos.request.BarberRequest;
 import org.cut_and_trim.dtos.response.BarberResponse;
 
 public interface BarberService {
-    
+
     BarberResponse register(BarberRequest barberRequest);
+
+    List<BarberResponse> findAll();
+
+    BarberResponse findById(UUID barberID);
 
 }
--- src/main/java/org/cut_and_trim/services/barber/BarberServiceImplementation.java
+++ src/main/java/org/cut_and_trim/services/barber/BarberServiceImplementation.java
@@ -1,10 +1,14 @@
 package org.cut_and_trim.services.barber;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.cut_and_trim.dtos.request.BarberRequest;
 import org.cut_and_trim.dtos.response.BarberResponse;
 import org.cut_and_trim.models.Barber;
 import org.cut_and_trim.models.BarberShop;
 import org.cut_and_trim.repositories.BarberRepository;
+import org.cut_and_trim.repositories.BarberShopRepository;
 import org.cut_and_trim.utils.BarberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,22 +20,37 @@ public class BarberServiceImplementation implements BarberService {
     private BarberRepository barberRepository;
 
     @Autowired
+    private BarberShopRepository barberShopRepository;
+
+    @Autowired
     private BarberMapper barberMapper;
 
     @Override
     public BarberResponse register(BarberRequest barberRequest) {
-        if (barberRepository.findByLogin(barberRequest.getLogin()).isPresent()
+        if (barberRepository.findByUsername(barberRequest.getUsername()).isPresent()
                 || barberRepository.findByEmail(barberRequest.getEmail()).isPresent())
             return null;
 
         Barber barber = barberMapper.toBarber(barberRequest);
-        BarberShop barberShop = new BarberShop(barber, null);
+        BarberShop barberShop = new BarberShop();
 
         barber.setBarberShop(barberShop);
 
         barberRepository.save(barber);
+        barberShopRepository.save(barberShop);
 
         return barberMapper.toBarberResponse(barber);
     }
 
+    @Override
+    public BarberResponse findById(UUID barberID) {
+        Barber barber = barberRepository.findById(barberID).orElse(null);
+        if(barber == null) return null;
+        return barberMapper.toBarberResponse(barber);
+    }
+
+    @Override
+    public List<BarberResponse> findAll() {
+        return barberMapper.toBarbersList(barberRepository.findAll());
+    }
 }
