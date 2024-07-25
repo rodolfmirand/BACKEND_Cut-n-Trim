b4e7e0d7317ef1842ceb933cdf8cc90a2e45052b
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-25 11:56:49 -0300

    refactor: renomeação da função findByLogin para findByUsername

--- src/main/java/org/cut_and_trim/repositories/BarberRepository.java
+++ src/main/java/org/cut_and_trim/repositories/BarberRepository.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BarberRepository extends JpaRepository<Barber, UUID>{
     
-    Optional<Barber> findByLogin(String login);
+    Optional<Barber> findByUsername(String login);
 
     Optional<Barber> findByEmail(String email);
 }
