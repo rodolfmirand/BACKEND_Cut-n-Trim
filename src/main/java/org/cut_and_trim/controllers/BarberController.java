b5f4716109f33e65a84a3469fd399239741ea0cd
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-25 12:00:26 -0300

    feat: criação da função findAll

--- src/main/java/org/cut_and_trim/controllers/BarberController.java
+++ src/main/java/org/cut_and_trim/controllers/BarberController.java
@@ -1,10 +1,13 @@
 package org.cut_and_trim.controllers;
 
+import java.util.List;
+
 import org.cut_and_trim.dtos.request.BarberRequest;
 import org.cut_and_trim.dtos.response.BarberResponse;
 import org.cut_and_trim.services.barber.BarberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,12 @@ public class BarberController {
     private BarberService barberService;
 
     @PostMapping("/register")
-    private ResponseEntity<BarberResponse> register(@RequestBody BarberRequest barberRequest){
+    public ResponseEntity<BarberResponse> register(@RequestBody BarberRequest barberRequest){
         return ResponseEntity.ok().body(barberService.register(barberRequest));
     } 
+
+    @GetMapping("/findall")
+    public ResponseEntity<List<BarberResponse>> findAll(){
+        return ResponseEntity.ok().body(barberService.findAll());
+    }
 }
