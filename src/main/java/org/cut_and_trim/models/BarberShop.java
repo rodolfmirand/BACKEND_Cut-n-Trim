a82fe622df59f55c47c33d850e945e7b94546be8
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-26 15:07:24 -0300

    refactor: adição da tag @Column nas variáveis

--- src/main/java/org/cut_and_trim/models/BarberShop.java
+++ src/main/java/org/cut_and_trim/models/BarberShop.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,13 +19,21 @@ import lombok.Data;
 public class BarberShop {
     
     @Id
+    @Column(name = "id", nullable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @Column(name = "name", nullable = false)
+    private String name;
 
     @OneToMany(cascade = CascadeType.ALL)
-    private List<Client> clients;
+    private List<Customer> customers;
 
     @Builder
     public BarberShop(){
+    }
+
+    public void addCustomerInList(Customer customer){
+        this.customers.add(customer);
     }
 }
