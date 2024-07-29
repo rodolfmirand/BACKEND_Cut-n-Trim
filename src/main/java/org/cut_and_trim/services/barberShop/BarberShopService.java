85cca2aaa58cdf6f819e0c0c16fc939282f9e439
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-29 14:22:28 -0300

    feat: criação dos métodos de adição de serviço e mostrar todos os serviços

--- src/main/java/org/cut_and_trim/services/barberShop/BarberShopService.java
+++ src/main/java/org/cut_and_trim/services/barberShop/BarberShopService.java
@@ -1,10 +1,19 @@
 package org.cut_and_trim.services.barberShop;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.cut_and_trim.dtos.request.BarberShopRequest;
 import org.cut_and_trim.dtos.response.BarberShopResponse;
+import org.cut_and_trim.dtos.response.BarberShopResponseServiceList;
+import org.cut_and_trim.dtos.response.ServiceResponse;
 
 public interface BarberShopService {
     
     BarberShopResponse register(BarberShopRequest barberShopRequest);
+
+    ServiceResponse addServiceInList(UUID barberShopID, UUID serviceID);
+
+    List<BarberShopResponseServiceList> findAllServices();
 
 }
--- src/main/java/org/cut_and_trim/services/barberShop/BarberShopServiceImplementation.java
+++ src/main/java/org/cut_and_trim/services/barberShop/BarberShopServiceImplementation.java
@@ -1,17 +1,24 @@
 package org.cut_and_trim.services.barberShop;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.cut_and_trim.dtos.request.BarberShopRequest;
 import org.cut_and_trim.dtos.response.BarberShopResponse;
+import org.cut_and_trim.dtos.response.BarberShopResponseServiceList;
+import org.cut_and_trim.dtos.response.ServiceResponse;
 import org.cut_and_trim.models.Barber;
 import org.cut_and_trim.models.BarberShop;
+import org.cut_and_trim.models.Service;
 import org.cut_and_trim.repositories.BarberRepository;
 import org.cut_and_trim.repositories.BarberShopRepository;
+import org.cut_and_trim.repositories.ServiceRepository;
 import org.cut_and_trim.utils.BarberShopMapper;
+import org.cut_and_trim.utils.ServiceMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class BarberShopServiceImplementation implements BarberShopService{
+@org.springframework.stereotype.Service
+public class BarberShopServiceImplementation implements BarberShopService {
 
     @Autowired
     private BarberShopRepository barberShopRepository;
@@ -22,21 +29,51 @@ public class BarberShopServiceImplementation implements BarberShopService{
     @Autowired
     private BarberRepository barberRepository;
 
+    @Autowired
+    private ServiceRepository serviceRepository;
+
+    @Autowired
+    private ServiceMapper serviceMapper;
+
     @Override
     public BarberShopResponse register(BarberShopRequest barberShopRequest) {
         Barber barber = barberRepository.findById(barberShopRequest.getBarberID()).orElse(null);
 
-        if(barber == null)
+        if (barber == null)
             return null;
-        
+
         BarberShop barberShop = barberShopMapper.toBarberShop(barberShopRequest);
 
         barber.setBarberShop(barberShop);
 
         barberShopRepository.save(barberShop);
         barberRepository.save(barber);
-        
+
         return barberShopMapper.toBarberShopResponse(barberShop);
     }
-    
+
+    @Override
+    public ServiceResponse addServiceInList(UUID barberShopID, UUID serviceID) {
+        BarberShop barberShop = barberShopRepository.findById(barberShopID).orElse(null);
+
+        if (barberShop == null)
+            return null;
+
+        Service service = serviceRepository.findById(serviceID).orElse(null);
+
+        if(service == null)
+            return null;
+
+        barberShop.addServiceInList(service);
+
+        barberShopRepository.save(barberShop);
+
+        return serviceMapper.toServiceResponse(service);
+    }
+
+    @Override
+    public List<BarberShopResponseServiceList> findAllServices() {
+        return barberShopMapper.toAllBarberShopsServicesList(barberShopRepository.findAll());
+    }
+
 }
