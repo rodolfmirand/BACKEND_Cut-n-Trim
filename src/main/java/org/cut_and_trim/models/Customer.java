39b1ee28f2719916a13766d258b375eaf4c8ccb3
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-29 13:21:29 -0300

    refactor: remoção do atirbuto referente a barbearia

--- src/main/java/org/cut_and_trim/models/Customer.java
+++ src/main/java/org/cut_and_trim/models/Customer.java
@@ -3,7 +3,6 @@ package org.cut_and_trim.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Data;
 
@@ -19,21 +18,16 @@ public class Customer {
     @Column(name = "name", nullable = true)
     private String name;
 
-    @ManyToOne
-    private BarberShop barberShop;
-
     @Builder
-    public Customer(String phoneNumber, String name, BarberShop barberShop){
+    public Customer(String phoneNumber, String name) {
         this.phoneNumber = phoneNumber;
         this.name = name;
-        this.barberShop = barberShop;
     }
 
     @Builder
     public Customer(Customer customer) {
         this.phoneNumber = customer.getPhoneNumber();
         this.name = customer.getName();
-        this.barberShop = customer.getBarberShop();
     }
 
     @Builder
