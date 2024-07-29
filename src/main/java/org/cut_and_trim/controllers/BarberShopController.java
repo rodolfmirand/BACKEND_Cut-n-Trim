5aee9be8b289766ecbf51cd480814d4c24453f08
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-29 14:12:50 -0300

    feat: criação dos maps para adicionar serviço e mostrar todos os serviços

--- src/main/java/org/cut_and_trim/controllers/BarberShopController.java
+++ src/main/java/org/cut_and_trim/controllers/BarberShopController.java
@@ -1,11 +1,17 @@
 package org.cut_and_trim.controllers;
 
+import java.util.List;
+
 import org.cut_and_trim.dtos.request.BarberShopRequest;
+import org.cut_and_trim.dtos.request.BarberShopRequestAddService;
 import org.cut_and_trim.dtos.response.BarberShopResponse;
+import org.cut_and_trim.dtos.response.BarberShopResponseServiceList;
+import org.cut_and_trim.dtos.response.ServiceResponse;
 import org.cut_and_trim.services.barberShop.BarberShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +26,15 @@ public class BarberShopController {
     @PostMapping("/register")
     public ResponseEntity<BarberShopResponse> register(@RequestBody BarberShopRequest barberShopRequest){
         return ResponseEntity.ok().body(barberShopService.register(barberShopRequest));
+    }
+
+    @PostMapping("/add-service")
+    public ResponseEntity<ServiceResponse> addService(@RequestBody BarberShopRequestAddService barberShopRequestAddService){
+        return ResponseEntity.ok().body(barberShopService.addServiceInList(barberShopRequestAddService.getBarberShopID(), barberShopRequestAddService.getServiceID()));
+    }
+
+    @GetMapping("/all-services")
+    public ResponseEntity<List<BarberShopResponseServiceList>> findAllServices(){
+        return ResponseEntity.ok().body(barberShopService.findAllServices());
     }
 }
