34faa3ffc0cba57482a90f585a22aa47d91f26a5
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-29 14:25:26 -0300

    feat: criação dos métodos que retornam lista de serviços

--- src/main/java/org/cut_and_trim/utils/BarberShopMapper.java
+++ src/main/java/org/cut_and_trim/utils/BarberShopMapper.java
@@ -1,7 +1,11 @@
 package org.cut_and_trim.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.cut_and_trim.dtos.request.BarberShopRequest;
 import org.cut_and_trim.dtos.response.BarberShopResponse;
+import org.cut_and_trim.dtos.response.BarberShopResponseServiceList;
 import org.cut_and_trim.models.BarberShop;
 import org.springframework.stereotype.Component;
 
@@ -22,5 +26,19 @@ public class BarberShopMapper {
 
     public BarberShopResponse toBarberShopResponse(BarberShop barberShop) {
         return new BarberShopResponse(barberShop);
+    }
+
+    public BarberShopResponseServiceList toBarberShopResponseServiceList(BarberShop barberShop){
+        return new BarberShopResponseServiceList(barberShop);
+    }
+
+    public List<BarberShopResponseServiceList> toAllBarberShopsServicesList(List<BarberShop> barberShops){
+        List<BarberShopResponseServiceList> barberShopsList = new ArrayList<BarberShopResponseServiceList>();
+
+        for(BarberShop barberShop : barberShops){
+            barberShopsList.add(toBarberShopResponseServiceList(barberShop));
+        }
+        
+        return barberShopsList;
     }
 }
