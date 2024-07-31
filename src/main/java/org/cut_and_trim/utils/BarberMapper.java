88491ad668dd87bac0598133a622b8d662e6781d
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-31 11:11:02 -0300

    feat: função para transformar Classe Barbeiro para Classe DTO BarberRegisterResponse

--- src/main/java/org/cut_and_trim/utils/BarberMapper.java
+++ src/main/java/org/cut_and_trim/utils/BarberMapper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.cut_and_trim.dtos.request.BarberRequest;
+import org.cut_and_trim.dtos.response.BarberRegisterResponse;
 import org.cut_and_trim.dtos.response.BarberResponse;
 import org.cut_and_trim.models.Barber;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,10 @@ public class BarberMapper {
 
     public BarberResponse toBarberResponse(Barber barber) {
         return new BarberResponse(barber);
+    }
+
+    public BarberRegisterResponse toBarberRegisterResponse(Barber barber){
+        return new BarberRegisterResponse(barber);
     }
 
     public List<BarberResponse> toBarbersList(List<Barber> barbersList){
