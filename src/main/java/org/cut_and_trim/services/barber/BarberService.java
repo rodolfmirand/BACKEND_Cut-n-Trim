640b39dd5990df559b6e23445bb9ea81824907dd
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-07-31 11:10:21 -0300

    refactor: alteração da lógica de registro do Barbeiro e Barbearia
    Agora a barbearia é registrado junto com o barbeiro

--- src/main/java/org/cut_and_trim/services/barber/BarberService.java
+++ src/main/java/org/cut_and_trim/services/barber/BarberService.java
@@ -3,12 +3,13 @@ package org.cut_and_trim.services.barber;
 import java.util.List;
 import java.util.UUID;
 
-import org.cut_and_trim.dtos.request.BarberRequest;
+import org.cut_and_trim.dtos.request.BarberRegisterRequest;
+import org.cut_and_trim.dtos.response.BarberRegisterResponse;
 import org.cut_and_trim.dtos.response.BarberResponse;
 
 public interface BarberService {
 
-    BarberResponse register(BarberRequest barberRequest);
+    BarberRegisterResponse register(BarberRegisterRequest barberRegisterRequest);
 
     List<BarberResponse> findAll();
 
--- src/main/java/org/cut_and_trim/services/barber/BarberServiceImplementation.java
+++ src/main/java/org/cut_and_trim/services/barber/BarberServiceImplementation.java
@@ -3,11 +3,17 @@ package org.cut_and_trim.services.barber;
 import java.util.List;
 import java.util.UUID;
 
+import org.cut_and_trim.dtos.request.BarberRegisterRequest;
 import org.cut_and_trim.dtos.request.BarberRequest;
+import org.cut_and_trim.dtos.request.BarberShopRequest;
+import org.cut_and_trim.dtos.response.BarberRegisterResponse;
 import org.cut_and_trim.dtos.response.BarberResponse;
 import org.cut_and_trim.models.Barber;
+import org.cut_and_trim.models.BarberShop;
 import org.cut_and_trim.repositories.BarberRepository;
+import org.cut_and_trim.repositories.BarberShopRepository;
 import org.cut_and_trim.utils.BarberMapper;
+import org.cut_and_trim.utils.BarberShopMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,25 +26,38 @@ public class BarberServiceImplementation implements BarberService {
     @Autowired
     private BarberMapper barberMapper;
 
+    @Autowired
+    private BarberShopRepository barberShopRepository;
+
+    @Autowired
+    private BarberShopMapper barberShopMapper;
+
     @Override
-    public BarberResponse register(BarberRequest barberRequest) {
+    public BarberRegisterResponse register(BarberRegisterRequest barberRegisterRequest) {
+        BarberRequest barberRequest = barberRegisterRequest.getBarber();
+        BarberShopRequest barberShopRequest = barberRegisterRequest.getBarberShop();
+
         if (barberRepository.findByUsername(barberRequest.getUsername()).isPresent()
                 || barberRepository.findByEmail(barberRequest.getEmail()).isPresent())
             return null;
 
         Barber barber = barberMapper.toBarber(barberRequest);
-        
-        barber.setBarberShop(null);
-        barberRepository.save(barber);
-        
+        BarberShop barberShop = barberShopMapper.toBarberShop(barberShopRequest);
 
-        return barberMapper.toBarberResponse(barber);
+        barber.setBarberShop(barberShop);
+
+        barberRepository.save(barber);
+
+        barberShopRepository.save(barberShop);
+
+        return barberMapper.toBarberRegisterResponse(barber);
     }
 
     @Override
     public BarberResponse findById(UUID barberID) {
         Barber barber = barberRepository.findById(barberID).orElse(null);
-        if(barber == null) return null;
+        if (barber == null)
+            return null;
         return barberMapper.toBarberResponse(barber);
     }
 
