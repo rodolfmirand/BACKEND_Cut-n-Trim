bdf32e1515b1d6ee0545c9ad194279577a9889f4
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-03 12:37:42 -0300

    feat: response para mostrar todos os serviços de uma barbearia

---
+++ src/main/java/org/cut_and_trim/dtos/response/BarberShopResponseServicesList.java
@@ -0,0 +1,26 @@
+package org.cut_and_trim.dtos.response;
+
+import org.cut_and_trim.models.BarberShop;
+import org.cut_and_trim.models.Service;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class BarberShopResponseServicesList {
+
+    private List<Service> services = new ArrayList<Service>();
+
+    public BarberShopResponseServicesList(BarberShop barberShop) {
+
+        if (barberShop.getServices().isEmpty()) {
+            this.services = null;
+        } else {
+            for (Service service : barberShop.getServices()) {
+                this.services.add(service);
+            }
+        }
+    }
+}
