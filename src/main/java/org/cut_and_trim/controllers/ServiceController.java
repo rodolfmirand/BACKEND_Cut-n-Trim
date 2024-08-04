0f3c2b22238dff28ecfce327ec792e7389046506
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-04 13:19:09 -0300

    refactor: mudança o map Delete de serviços

--- src/main/java/org/cut_and_trim/controllers/ServiceController.java
+++ src/main/java/org/cut_and_trim/controllers/ServiceController.java
@@ -1,17 +1,14 @@
 package org.cut_and_trim.controllers;
 
-import java.util.UUID;
-
+import org.cut_and_trim.dtos.request.ServiceDeleteRequest;
 import org.cut_and_trim.dtos.request.ServiceRequest;
 import org.cut_and_trim.dtos.request.ServiceUpdateRequest;
 import org.cut_and_trim.dtos.response.ServiceResponse;
-import org.cut_and_trim.models.Service;
 import org.cut_and_trim.services.service.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,8 +36,8 @@ public class ServiceController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> delete(@PathVariable(value = "id") UUID id){
-        if(!serviceService.delete(id)) return ResponseEntity.badRequest().body("Service not founded.");
+    public ResponseEntity<?> delete(@RequestBody ServiceDeleteRequest serviceDeleteRequest){
+        if(!serviceService.delete(serviceDeleteRequest)) return ResponseEntity.badRequest().body("Service not founded.");
         return ResponseEntity.ok().body("Service deleted successfully.");
     }
 }
