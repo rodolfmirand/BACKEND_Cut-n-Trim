8e14a0f770b89a889eb3c3c4289eac57c6c7c988
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-08 13:38:31 -0300

    refactor: alteração de atributos

--- src/main/java/org/cut_and_trim/dtos/response/BarberShopResponse.java
+++ src/main/java/org/cut_and_trim/dtos/response/BarberShopResponse.java
@@ -1,14 +1,17 @@
 package org.cut_and_trim.dtos.response;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import org.cut_and_trim.models.Barber;
 import org.cut_and_trim.models.BarberShop;
 
 import lombok.Getter;
 
 @Getter
 public class BarberShopResponse {
-    
+
     private UUID id;
 
     private String name;
@@ -24,8 +27,10 @@ public class BarberShopResponse {
     private String state;
 
     private String cep;
-    
-    public BarberShopResponse(BarberShop barberShop){
+
+    private List<BarberResponse> barbers = new ArrayList<>();
+
+    public BarberShopResponse(BarberShop barberShop) {
         this.id = barberShop.getId();
         this.name = barberShop.getName();
         this.street = barberShop.getStreet();
@@ -34,6 +39,8 @@ public class BarberShopResponse {
         this.city = barberShop.getCity();
         this.state = barberShop.getState();
         this.cep = barberShop.getCep();
+        for (Barber barber : barberShop.getBarbers()) {
+            this.barbers.add(new BarberResponse(barber));
+        }
     }
-
 }
