4bbfcf0314c33fdd4aacc427d8a13b954ce046db
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-09 12:36:43 -0300

    feat: maps para login de barbeiro e proprietario

--- src/main/java/org/cut_and_trim/controllers/LoginController.java
+++ src/main/java/org/cut_and_trim/controllers/LoginController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.cut_and_trim.dtos.request.SignupRequest;
 import org.cut_and_trim.dtos.response.SignupResponse;
 import org.cut_and_trim.services.barber.BarberService;
+import org.cut_and_trim.services.owner.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,15 +20,30 @@ public class LoginController {
     @Autowired
     private BarberService barberService;
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+    @Autowired
+    private OwnerService ownerService;
+
+    @PostMapping("/barber-signup")
+    public ResponseEntity<?> signupBarber(@RequestBody SignupRequest signupRequest) {
         if (signupRequest.getEmail().isEmpty() || signupRequest.getPassword().isEmpty())
             return ResponseEntity.badRequest().body("Empty Values.");
 
         UUID id = barberService.signUp(signupRequest);
 
         if (id == null)
-            return ResponseEntity.badRequest().body("Invalid signup.");
+            return ResponseEntity.badRequest().body("Invalid Signup.");
+
+        return ResponseEntity.ok().body(new SignupResponse(id));
+    }
+
+    @PostMapping("/owner-signup")
+    public ResponseEntity<?> signupOwner(@RequestBody SignupRequest signupRequest) {
+        if (signupRequest.getEmail().isEmpty() || signupRequest.getPassword().isEmpty())
+            return ResponseEntity.badRequest().body("Empty values.");
+
+        UUID id = ownerService.signup(signupRequest);
+
+        if (id == null) return ResponseEntity.badRequest().body("Invalid Signup.");
 
         return ResponseEntity.ok().body(new SignupResponse(id));
     }
