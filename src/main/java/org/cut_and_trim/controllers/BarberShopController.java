7b297b0476a4639daf24df9293ab373c51d3987e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-19 20:22:59 -0300

    feat: post map para upload de imagem

--- src/main/java/org/cut_and_trim/controllers/BarberShopController.java
+++ src/main/java/org/cut_and_trim/controllers/BarberShopController.java
@@ -1,5 +1,6 @@
 package org.cut_and_trim.controllers;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -9,11 +10,14 @@ import org.cut_and_trim.dtos.response.BarberShopResponse;
 import org.cut_and_trim.dtos.response.AllBarberShopsResponseServicesList;
 import org.cut_and_trim.dtos.response.BarberShopResponseServicesList;
 import org.cut_and_trim.dtos.response.ServiceResponse;
+import org.cut_and_trim.models.BarberShopImage;
 import org.cut_and_trim.services.barberShop.BarberShopService;
+import org.cut_and_trim.services.barberShopImage.BarberShopImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping(path = "/cutandtrim/barbershop")
@@ -21,6 +25,9 @@ public class BarberShopController {
 
     @Autowired
     private BarberShopService barberShopService;
+
+    @Autowired
+    private BarberShopImageService imageService;
 
     @PostMapping("/add-service")
     public ResponseEntity<ServiceResponse> addService(
@@ -35,7 +42,7 @@ public class BarberShopController {
     }
 
     @GetMapping("/all-sv-act/{id}")
-    public ResponseEntity<BarberShopResponseServicesList> findAllServicesActives(@PathVariable(value = "id") UUID id){
+    public ResponseEntity<BarberShopResponseServicesList> findAllServicesActives(@PathVariable(value = "id") UUID id) {
         return ResponseEntity.ok().body(barberShopService.findAllServicesActives(id));
     }
 
@@ -43,4 +50,16 @@ public class BarberShopController {
     public ResponseEntity<BarberShopResponseServicesList> findServices(@PathVariable(value = "id") UUID id) {
         return ResponseEntity.ok().body(barberShopService.findServices(id));
     }
+
+    @PostMapping("/upload-image")
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file, @RequestParam("id") UUID id) {
+        try {
+            BarberShopImage image = imageService.saveImage(file, id);
+            return ResponseEntity.ok().body("Image saved with success.");
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body("Error saving image.");
+        }
+    }
+
+
 }
