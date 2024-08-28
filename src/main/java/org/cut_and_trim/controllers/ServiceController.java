a105159308cdedf05e0f6146947b514a3808b944
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-27 22:15:58 -0300

    feat: map para encontrar servico por id

--- src/main/java/org/cut_and_trim/controllers/ServiceController.java
+++ src/main/java/org/cut_and_trim/controllers/ServiceController.java
@@ -8,36 +8,43 @@ import org.cut_and_trim.services.service.ServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping(path = "/cutandtrim/service")
 public class ServiceController {
-    
+
     @Autowired
     private ServiceService serviceService;
 
     @PostMapping("/register")
-    public ResponseEntity<ServiceResponse> register(@RequestBody ServiceRequest serviceRequest){
+    public ResponseEntity<ServiceResponse> register(@RequestBody ServiceRequest serviceRequest) {
         return ResponseEntity.ok().body(serviceService.register(serviceRequest));
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> update(@RequestBody ServiceUpdateRequest serviceUpdateRequest){
+    public ResponseEntity<?> update(@RequestBody ServiceUpdateRequest serviceUpdateRequest) {
         ServiceResponse serviceResponse = serviceService.update(serviceUpdateRequest);
-        
-        if(serviceResponse == null) return ResponseEntity.badRequest().body("Service not founded.");
+
+        if (serviceResponse == null) return ResponseEntity.badRequest().body("Service not founded.");
 
         return ResponseEntity.ok().body(serviceResponse);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<?> delete(@RequestBody ServiceDeleteRequest serviceDeleteRequest){
-        if(!serviceService.delete(serviceDeleteRequest)) return ResponseEntity.badRequest().body("Service not founded.");
+    public ResponseEntity<?> delete(@RequestBody ServiceDeleteRequest serviceDeleteRequest) {
+        if (!serviceService.delete(serviceDeleteRequest))
+            return ResponseEntity.badRequest().body("Service not founded.");
         return ResponseEntity.ok().body("Service deleted successfully.");
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<?> findById(@RequestParam("id") UUID id) {
+        ServiceResponse serviceResponse = serviceService.findById(id);
+        if (serviceResponse != null)
+            return ResponseEntity.ok().body(serviceResponse);
+        return ResponseEntity.badRequest().body("Service not exists.");
     }
 }
